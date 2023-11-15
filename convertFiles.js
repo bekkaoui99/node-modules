8bb8b9001928b194a6e61c292019316ad09bf59b
BekkaouiHamza <bekkaouihamza86@gmail.com>
2023-11-15 19:11:03 +0100

    recreate the converFiles module

--- convertFiles.js
+++ convertFiles.js
@@ -1,61 +1,63 @@
-// npm install mammoth pdf-parse docxtemplater
-// npm install pdf2pptx officegen
-
 const fs = require("fs");
-const path = require("path");
-const mammoth = require("mammoth");
-const pdfParse = require("pdf-parse");
-const pdf2pptx = require("pdf2pptx");
+const pdfPoppler = require("pdf-poppler");
 const officegen = require("officegen");
-const JSZip = require("jszip");
-
-// Function to convert Word to PDF
-async function convertWordToPDF(inputPath, outputPath) {
-  try {
-    const fileContent = fs.readFileSync(inputPath, "utf-8");
-    const { value } = await mammoth.extractRawText({
-      arrayBuffer: Buffer.from(fileContent),
-    });
-
-    // You may use another package like 'pdfkit' to create a PDF from the extracted text.
-    // For simplicity, let's save the raw text as a PDF for this example.
-    fs.writeFileSync(outputPath, value);
-    console.log("Word to PDF conversion successful.");
-  } catch (error) {
-    console.error("Error converting Word to PDF:", error.message);
-  }
-}
+const mammoth = require("mammoth");
 
 // Function to convert PDF to Word
 async function convertPDFToWord(inputPath, outputPath) {
   try {
-    const dataBuffer = fs.readFileSync(inputPath);
-    const data = await pdfParse(dataBuffer);
-
-    // Extract text content from PDF
-    const pdfText = data.text;
-
-    // Create a simple Word document with extracted text
-    const content = `<w:p><w:r><w:t>${pdfText}</w:t></w:r></w:p>`;
-    const template = new Docxtemplater();
-    template.loadZip(new JSZip(content));
-
-    // Save the Word document
-    const generatedDoc = template.getZip().generate({ type: "nodebuffer" });
-    fs.writeFileSync(outputPath, generatedDoc);
-
+    const images = await pdfPoppler.convert(inputPath, { format: "jpeg" });
+    const htmlContent = images
+      .map(
+        (image, index) =>
+          `<img src="data:image/jpeg;base64,${fs.readFileSync(
+            image,
+            "base64"
+          )}" alt="slide${index + 1}">`
+      )
+      .join("");
+    const options = { styleMap: "p => div" }; // Convert paragraphs to divs
+    const { value } = await mammoth.extractRawText(
+      { arrayBuffer: Buffer.from(htmlContent) },
+      options
+    );
+    fs.writeFileSync(outputPath, value);
     console.log("PDF to Word conversion successful.");
   } catch (error) {
     console.error("Error converting PDF to Word:", error.message);
   }
 }
 
+// Function to convert Word to PDF
+async function convertWordToPDF(inputPath, outputPath) {
+  try {
+    const fileContent = fs.readFileSync(inputPath, "utf-8");
+    const htmlContent = mammoth.extractRawText({
+      arrayBuffer: Buffer.from(fileContent),
+    });
+    // You may use another package like 'pdfkit' to create a PDF from the extracted text.
+    // For simplicity, let's save the raw text as a PDF for this example.
+    fs.writeFileSync(outputPath, htmlContent.value);
+    console.log("Word to PDF conversion successful.");
+  } catch (error) {
+    console.error("Error converting Word to PDF:", error.message);
+  }
+}
+
 // Function to convert PDF to PPT
 async function convertPDFToPPT(inputPath, outputPath) {
   try {
-    const pptx = await pdf2pptx(inputPath);
-    const pptxBuffer = await pptx.generate();
-    fs.writeFileSync(outputPath, pptxBuffer);
+    const images = await pdfPoppler.convert(inputPath, { format: "jpeg" });
+    const pptx = officegen("pptx");
+
+    for (let i = 0; i < images.length; i++) {
+      const imageBuffer = fs.readFileSync(images[i]);
+      const slide = pptx.makeNewSlide();
+      slide.addImage({ path: imageBuffer, type: "image/jpeg", x: 0, y: 0 });
+    }
+
+    pptx.generate(fs.createWriteStream(outputPath));
+
     console.log("PDF to PPT conversion successful.");
   } catch (error) {
     console.error("Error converting PDF to PPT:", error.message);
@@ -85,8 +87,8 @@ function convertPPTToPDF(inputPath, outputPath) {
 }
 
 module.exports = {
-  convertWordToPDF,
   convertPDFToWord,
+  convertWordToPDF,
   convertPDFToPPT,
   convertPPTToPDF,
 };
